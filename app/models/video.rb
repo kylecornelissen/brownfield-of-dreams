53dc1ed4bcc68f6a8af8f67f4408576daa3976ea
Aurie <Myrdden42@gmail.com>
2019-07-05 19:02:25 -0600

    Add position validation to Video

--- app/models/video.rb
+++ app/models/video.rb
@@ -4,4 +4,17 @@ class Video < ApplicationRecord
   has_many :user_videos
   has_many :users, through: :user_videos
   belongs_to :tutorial
+
+  validates :position, numericality: {only_integer: true}
+  # validates :position, numericality: {only_integer: true}, if: :next_in_line
+
+  # def next_in_line
+  #   binding.pry
+  #   vids = tutorial.videos
+  #   if vids.empty?
+  #     position == 0
+  #   else
+  #     position == (tutorial.videos.max_by(&:position).position + 1)
+  #   end
+  # end
 end
