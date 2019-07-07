1efb0d78bd8bb51e39d9af9971fe48e91eae7ee3
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-07-07 06:10:38 -0600

    Add render_followers method to github_facade

--- app/facades/github_facade.rb
+++ app/facades/github_facade.rb
@@ -1,9 +1,10 @@
 class GithubFacade
-  attr_reader :render
+  attr_reader :render, :render_followers
   def initialize(user)
     @user = user
     if user.token != nil
       @render = "partials/github_repos"
+      @render_followers = "partials/github_followers"
     else
       @render = "partials/github_auth_prompt"
     end
@@ -16,7 +17,14 @@ class GithubFacade
       repos = GithubService.new(@user).repo_info
     end
     repos.map do |repo|
-      GithubRepo.new(repo)            
+      GithubRepo.new(repo)
+    end
+  end
+
+  def followers
+    followers = GithubService.new(@user).follower_info
+    followers.map do |follower|
+      GithubFollower.new(follower)
     end
   end
 end
