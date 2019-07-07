6d1e65fa6427ca531d92cea0c86376f6675efda8
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-07-07 06:11:35 -0600

    Add github_followers partial to views

---
+++ spec/features/user/user_can_see_github_followers_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+feature 'user can see github followers' do
+  before :each do
+    @user = create(:user)
+    @user_with_token = create(:user, token: "a343434234324t3r")
+  end
+
+  scenario 'it can login and see followers with token' do
+    VCR.use_cassette("services/github_service") do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user_with_token)
+
+      visit '/dashboard'
+
+      expect(current_path).to eq('/dashboard')
+      expect(page).to have_css('#github_followers')
+
+      within('#github_followers') do
+        page.assert_selector('.follower', count: 5)
+        within(first('.follower')) do
+          expect(page).to have_content('test_user/follower1')
+        end
+      end
+    end
+  end
+
+  scenario 'it can login and will not see followers without token' do
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+
+    visit '/dashboard'
+
+    expect(current_path).to eq('/dashboard')
+    expect(page).to_not have_css('#github_followers')
+  end
+end
