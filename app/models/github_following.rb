d4f9bdc95d3da7e5ff202f73e7543e8264508782
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-07-08 16:19:58 -0600

    Merged follower and following models into one github_handle model
    Co-Authored-By: Aurie <Myrdden@users.noreply.github.com>

--- app/models/github_following.rb
+++
@@ -1,7 +0,0 @@
-class GithubFollowing
-  attr_reader :path, :login_name
-  def initialize(data = {})
-    @path = data[:html_url]
-    @login_name = data[:login]
-  end
-end
--- app/models/github_handle.rb
+++ app/models/github_handle.rb
@@ -1,4 +1,4 @@
-class GithubFollower
+class GithubHandle
   attr_reader :path, :login_name
   def initialize(data = {})
     @path = data[:html_url]
