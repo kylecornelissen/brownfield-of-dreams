efa575d17c31f0a74525d8919b92854d304a00d4
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-07-08 15:08:06 -0600

    Update following test with github-test-key

--- spec/features/user/user_can_see_github_following_spec.rb
+++ spec/features/user/user_can_see_github_following_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 feature 'user can see github following' do
   before :each do
     @user = create(:user)
-    @user_with_token = create(:user, token: "a343434234324t3r")
+    @user_with_token = create(:user, token: ENV["github-test-key"])
   end
 
   scenario 'it can login and see following with token' do
@@ -16,9 +16,9 @@ feature 'user can see github following' do
       expect(page).to have_css('#github_following')
 
       within('#github_following') do
-        page.assert_selector('.following', count: 5)
+        page.assert_selector('.following')
         within(first('.following')) do
-          expect(page).to have_content('test_user/following1')
+          expect(page).to have_content('rwarbelow')
         end
       end
     end
