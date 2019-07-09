74d8f2b8437cdf296bbb01b81c6a0868681cf949
Aurie <Myrdden42@gmail.com>
2019-07-09 15:08:27 -0600

    Add flash to friendship create
    Co-authored-by: kylecornelissen <kyle.cornelissen@gmail.com>

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,7 +1,9 @@
 class FriendshipsController < ApplicationController
   def create
-    friend = User.find_by(uid: params[:uid])
-    Friendship.create(user_id: current_user.id, friend_id: friend.id)
+    friend = Friendship.new(user_id: current_user.id, friend_id: User.find_by(uid: params[:uid]))
+    if !friend.save
+      flash[:error] = "Invalid user id."
+    end
     redirect_to dashboard_path
   end
 end
