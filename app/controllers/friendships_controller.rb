350682a3af7ffa0c8872538de89313941643b208
Aurie <Myrdden42@gmail.com>
2019-07-09 14:50:46 -0600

    Fixed friendships, added friends list
    Co-authored-by: kylecornelissen <kyle.cornelissen@gmail.com>

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,5 +1,7 @@
 class FriendshipsController < ApplicationController
   def create
-
+    friend = User.find_by(uid: params[:uid])
+    Friendship.create(user_id: current_user.id, friend_id: friend.id)
+    redirect_to dashboard_path
   end
 end
