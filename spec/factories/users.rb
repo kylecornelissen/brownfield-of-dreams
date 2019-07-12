3a8c120ca13884ac3889f7ffd8d3e85e68330093
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-07-12 13:51:32 -0600

    Remove fakers from attributes
    Co-Authored-By: Aurie <Myrdden@users.noreply.github.com>

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :user do
     email { Faker::Internet.email }
-    first_name { Faker::Dog.name }
+    first_name { 'hello' }
     last_name { Faker::Artist.name }
     password { Faker::Color.color_name }
     role { :default }
