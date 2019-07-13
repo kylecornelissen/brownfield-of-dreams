dd41c92784ef9bcba1d3c21705395099167bd879
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-07-13 11:30:36 -0600

    Update app/controllers/friendships_controller.rb
    Okay I tried it and that method is better. Thank you!
    
    Co-Authored-By: Aurie <myrdden42@gmail.com>

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,7 +1,7 @@
 class FriendshipsController < ApplicationController
   def create
     friend = User.find_by(uid: params[:uid])
-    Friendship.create(user_id: current_user.id, friend_id: friend.id)
+    friend = Friendship.new(user_id: current_user.id, friend_id: User.find_by(uid: params[:uid]).id)
     if !friend.save
       flash[:error] = "Invalid user id."
     end
