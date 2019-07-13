f32e5b020ffe33df9f9578eb7030d8f5a89f2a90
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-07-13 16:57:03 -0600

    Change create method in videos_controller

--- app/controllers/admin/videos_controller.rb
+++ app/controllers/admin/videos_controller.rb
@@ -11,13 +11,15 @@ class Admin::VideosController < Admin::BaseController
   end
 
   def create
-    tutorial  = Tutorial.find(params[:tutorial_id])
-    thumbnail = YouTube::Video.by_id(new_video_params[:video_id]).thumbnail
-    video     = tutorial.videos.new(new_video_params.merge(thumbnail: thumbnail))
+    begin
+      tutorial  = Tutorial.find(params[:tutorial_id])
+      thumbnail = YouTube::Video.by_id(new_video_params[:video_id]).thumbnail
+      video     = tutorial.videos.new(new_video_params.merge(thumbnail: thumbnail))
 
-    if video.save
+      video.save
+
       flash[:success] = 'Successfully created video.'
-    else #rescue StandardError # Sorry about this. We should get more specific instead of swallowing all errors.
+    rescue StandardError # Sorry about this. We should get more specific instead of swallowing all errors.
       flash[:error] = 'Unable to create video.'
     end
     redirect_to edit_admin_tutorial_path(id: tutorial.id)
