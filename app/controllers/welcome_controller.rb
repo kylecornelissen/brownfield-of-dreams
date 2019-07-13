7176a3891a58db985652e73321a5e087376fbd93
Aurie <Myrdden42@gmail.com>
2019-07-13 13:17:00 -0600

    More rubocop fixed

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,18 +2,17 @@
 
 class WelcomeController < ApplicationController
   def index
-    @tutorials = if current_user
-                   if params[:tag]
-                     Tutorial.tagged_with(params[:tag]).paginate(page: params[:page], per_page: 5)
-                   else
-                     Tutorial.all.paginate(page: params[:page], per_page: 5)
-                                end
-                 else
-                   if params[:tag]
-                     Tutorial.no_classroom.tagged_with(params[:tag]).paginate(page: params[:page], per_page: 5)
-                   else
-                     Tutorial.no_classroom.all.paginate(page: params[:page], per_page: 5)
-                                end
-                 end
+    @tutorials =
+      if current_user
+        if params[:tag]
+          Tutorial.tagged_with(params[:tag]).paginate(page: params[:page], per_page: 5)
+        else
+          Tutorial.all.paginate(page: params[:page], per_page: 5)
+        end
+      elsif params[:tag]
+        Tutorial.no_classroom.tagged_with(params[:tag]).paginate(page: params[:page], per_page: 5)
+      else
+        Tutorial.no_classroom.all.paginate(page: params[:page], per_page: 5)
+      end
   end
 end
