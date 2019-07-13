525a3f20cbabebfce0f76681984ce684b42ee5be
Aurie <myrdden42@gmail.com>
2019-07-13 11:34:54 -0600

    Remove unused line

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,6 +1,5 @@
 class FriendshipsController < ApplicationController
   def create
-    friend = User.find_by(uid: params[:uid])
     friend = Friendship.new(user_id: current_user.id, friend_id: User.find_by(uid: params[:uid]).id)
     if !friend.save
       flash[:error] = "Invalid user id."
