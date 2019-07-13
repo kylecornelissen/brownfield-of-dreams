bfad42dc80c5822f7982b0063b657d4e36c805aa
Aurie <myrdden42@gmail.com>
2019-07-13 16:50:54 -0600

    Apply suggestions from code review

--- spec/requests/api/v1/videos_request_spec.rb
+++ spec/requests/api/v1/videos_request_spec.rb
@@ -5,15 +5,15 @@ require 'rails_helper'
 describe 'Videos API' do
   it 'sends a single tutorial' do
     tutorial = create(:tutorial)
-    video1 = create(:video, tutorial_id: tutorial.id)
+    video = create(:video, tutorial_id: tutorial.id)
     create(:video, tutorial_id: tutorial.id)
 
-    get "/api/v1/videos/#{video1.id}"
+    get "/api/v1/videos/#{video.id}"
 
     expect(response).to be_successful
 
     parsed = JSON.parse(response.body, symbolize_names: true)
 
-    expect(parsed[:id]).to eq(video1.id)
+    expect(parsed[:id]).to eq(video.id)
   end
 end
